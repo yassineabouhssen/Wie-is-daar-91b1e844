9b044d69831ab9dfbe8f44a039fd98681f027c63
Yassine_Abouhssen <2078683@talnet.nl>
2020-04-23 14:23:05 +0200

    Uitwerking van de exercise

--- groet.php
+++ groet.php
@@ -2,10 +2,12 @@
 
 echo  "hallo  wat is uw naam?". "\n";
 
+$naam= readline("Enter a name: ");
+echo "wat is uw naam: ".$naam;
+// $input = trim(fgets(STDIN, 1024));
 
-$input = trim(fgets(STDIN, 1024));
+// echo  $input,"\n" ; 
 
-echo  $input,"\n" ; 
 
 
 ?>
